bd2ffe5039d10f09791c2d2524df7496742a720e
Spurtinath <spurtigowda@gmail.com>
2017-02-11 21:11:06 -0800

    first python code

---
+++ python1.py
@@ -0,0 +1,9 @@
+
+
+#this is our first python code
+
+print "hi spurtinath"
+
+
+this is similar to our C code, justs print wat is inside the paranthesis, it can be either single quotes or double quotes
+
